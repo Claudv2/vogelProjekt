e884eff3aab541b1defd886523b2670d596c89cc
Claudio Frisenna <claudio.frisenna@merkleinc.com>
2022-12-21 09:48:18 +0100

    init

---
+++ projekt/projekt/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using projekt.Models;
+
+namespace projekt.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var data = new List<Zugskompositionen> {
+                new Zugskompositionen(new Rollmaterial()),
+                new Zugskompositionen(new Rollmaterial())
+            };
+               
+            return View(data);
+        }
+            
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Zugskompositionen()
+        {
+            // Retrieve data from tables and create list of MyModel objects
+            var data = new List<Zugskompositionen> {
+                new Zugskompositionen(new Rollmaterial()),
+                new Zugskompositionen(new Rollmaterial())
+            };
+
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ projekt/projekt/Models/Zugskompositionen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+
+namespace projekt.Models
+{
+    public class Zugskompositionen
+    {
+        public Guid id;
+
+        public Rollmaterial locke;
+        public Zugskompositionen(Rollmaterial locke1) { 
+            id = new Guid(); 
+            locke = locke1; 
+        }
+        
+    }
+
+    public class Rollmaterial
+    {
+        public Guid id;
+        public Rollmaterial() { id = new Guid(); }
+        public string ImBesitz { get; set; }
+        public string Kaufpreis { get; set; }
+        public string Verkaeufer { get; set; }
+        public string Schaetzpreis { get; set; }
+        public string Hersteller { get; set; }
+        public string ArtNr { get; set; }
+        public string SetNr { get; set; }
+        public string Bezeichung { get; set; }
+        public string Bemerkung { get; set; }
+        public string Bahngesellschaft { get; set; }
+        public string Loktyp { get; set; }
+        public string Loknummer { get; set; }
+        public string Veroeffentlichung { get; set; }
+    }
+
+}
