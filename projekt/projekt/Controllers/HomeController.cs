4fa6a76786adf6ac6eba7d3e7ce9851d8489a0bf
Claudio Frisenna <claudio.frisenna@merkleinc.com>
2022-12-22 14:26:31 +0100

    rollmaterial

--- projekt/projekt/Controllers/HomeController.cs
+++ projekt/projekt/Controllers/HomeController.cs
@@ -12,8 +12,8 @@ namespace projekt.Controllers
         public ActionResult Index()
         {
             var data = new List<Zugskompositionen> {
-                new Zugskompositionen(new Rollmaterial()),
-                new Zugskompositionen(new Rollmaterial())
+                new Zugskompositionen(new Rollmaterial("2022", 698.00, "MBL", "", "LGB", "36685", "01", "Salonwagen", "Creme-Blau","Pullman Express", "RhB", "As", 1141, "2022")),
+                new Zugskompositionen(new Rollmaterial("2022", 698.00, "MBL", "", "LGB", "36685", "02", "Salonwagen", "Creme-Blau","Pullman Express", "RhB", "As", 1141, "2022"))
             };
                
             return View(data);
@@ -29,9 +29,9 @@ namespace projekt.Controllers
         public ActionResult Rollmaterial()
         {
             // Retrieve data from tables and create list of MyModel objects
-            var data = new List<Zugskompositionen> {
-                new Zugskompositionen(new Rollmaterial()),
-                new Zugskompositionen(new Rollmaterial())
+            var data = new List<Rollmaterial> {
+                new Rollmaterial("2022", 698.00, "MBL", "", "LGB", "36685", "01", "Salonwagen", "Creme-Blau","Pullman Express", "RhB", "As", 1141, "2022"),
+                new Rollmaterial("2022", 698.00, "MBL", "", "LGB", "36685", "02", "Salonwagen", "Creme-Blau","Pullman Express", "RhB", "As", 1141, "2022")
             };
 
             return View(data);
--- projekt/projekt/Models/Zugskompositionen.cs
+++ projekt/projekt/Models/Zugskompositionen.cs
@@ -21,20 +21,53 @@ namespace projekt.Models
     public class Rollmaterial
     {
         public Guid id;
-        public Rollmaterial() { id = new Guid(); }
         public string ImBesitz { get; set; }
-        public string Kaufpreis { get; set; }
+        public double Kaufpreis { get; set; }
         public string Verkaeufer { get; set; }
         public string Schaetzpreis { get; set; }
         public string Hersteller { get; set; }
         public string ArtNr { get; set; }
         public string SetNr { get; set; }
         public string Bezeichung { get; set; }
+        public string Farbe { get; set; }
         public string Bemerkung { get; set; }
         public string Bahngesellschaft { get; set; }
         public string Loktyp { get; set; }
-        public string Loknummer { get; set; }
-        public string Veroeffentlichung { get; set; }
+        public int Loknummer { get; set; }
+        public string Veroeffentlichung { get; set;}
+        public Rollmaterial(
+            string paramImBesitz,
+            double paramKaufpreis,
+            string paramVerkaeufer,
+            string paramSchaetzpreis,
+            string paramHersteller,
+            string paramArtNr,
+            string paramSetNr,
+            string paramBezeichung,
+            string paramBemerkung,
+            string paramFarbe,
+            string paramBahngesellschaft,
+            string paramLoktyp,
+            int paramLoknummer,
+            string paramVeroeffentlichung
+        ) { 
+            id = new Guid(); 
+            ImBesitz = paramImBesitz;
+            Kaufpreis = paramKaufpreis;
+            Verkaeufer = paramVerkaeufer;
+            Schaetzpreis = paramSchaetzpreis;
+            Hersteller = paramHersteller;
+            ArtNr = paramArtNr;
+            SetNr = paramSetNr;
+            Bezeichung = paramBezeichung;
+            Bemerkung = paramBemerkung;
+            Farbe = paramFarbe;
+            Bahngesellschaft = paramBahngesellschaft;
+            Loktyp = paramLoktyp;
+            Loknummer = paramLoknummer;
+            Veroeffentlichung = paramVeroeffentlichung;
+        }
+
     }
 
 }
