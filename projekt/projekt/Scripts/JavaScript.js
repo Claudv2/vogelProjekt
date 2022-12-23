86a298e0808914a544afff9798e50829f86268a1
Claudio Frisenna <claudio.frisenna@merkleinc.com>
2022-12-23 22:23:53 +0100

    all buttons and light dark mode

--- projekt/projekt/Scripts/JavaScript.js
+++ projekt/projekt/Scripts/JavaScript.js
@@ -1,14 +1,37 @@
 ﻿$(document).ready(function () {
-    var mode = "light";
 
+    if (!sessionStorage.getItem('mode')) {
+        sessionStorage.setItem('mode', 'light')
+    }
 
-    $('#mode').click(function () {
-        if (mode === "light") {
-            mode = "dark";
-            document.getElementsByClassName('.table').style.backgroundColor = "white";
-            $('.table').style.color = "#2c2c2c";
-            console.log("asdadad")
+    console.log('asdasdasd')
+    $('#modeButton').click(function () { 
+        if (sessionStorage.getItem('mode') === "light") {
+            sessionStorage.setItem('mode', 'dark')
         }
+        else {
+            sessionStorage.setItem('mode', 'light')
+        }
+        getColor()
     });
+
+    getColor();
+
+    function getColor() {
+        if (sessionStorage.getItem('mode') === "dark") {
+            document.body.style.backgroundColor = "#2c2c2c";
+            document.body.style.color = "white";
+            document.getElementById('modeButton').style.color = "#2c2c2c"
+            document.getElementById('addButton').style.color = "#2c2c2c"
+        }
+        else if (sessionStorage.getItem('mode') === "light") {
+            document.body.style.backgroundColor = "white";
+            document.body.style.color = "#2c2c2c";
+            document.getElementById('modeButton').style.color = "#2c2c2c"
+            document.getElementById('addButton').style.color = "#2c2c2c"
+        }
+    }
 });
+
+    
 
